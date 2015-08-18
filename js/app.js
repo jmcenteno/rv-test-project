b9b25220427e9d5d986a6871338a09e4286b5027
Jose Centeno <jmcenteno@gmail.com>
2015-08-18 13:03:17 -0400

    - users factory

--- js/app.js
+++ js/app.js
@@ -51,3 +51,32 @@ app.run(['$rootScope',
         
     }
 ]);
+
+/*
+ * _users Factory
+ *
+ * AngularJS service to request user data from remote API
+ * @returns     (object) An object with methods to obtain collections or single records
+ */
+app.factory('_users', ['$http', 'API_URL',
+    function($http, API_URL) {
+        
+        var users = {};
+        
+        users.getAllUsers = function() {
+            return $http.get(API_URL + '/users').then(function(response) {
+                return response.data;
+            });
+        };
+        
+        users.getUser = function(id) {
+            return $http.get(API_URL + '/users/' + id).then(function(response) {
+                return response.data;
+            });
+        };
+        
+        return users;
+        
+    }
+]);
+
