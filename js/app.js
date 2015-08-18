18c26aa4b43ecfb799a674c37b23ab7590dd779b
Jose Centeno <jmcenteno@gmail.com>
2015-08-18 13:02:43 -0400

    - define application routes

--- js/app.js
+++ js/app.js
@@ -5,7 +5,49 @@ var app = angular.module('WidgetSpa', [
 app.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
         
+        // declare application states (routes)
+        $stateProvider
         
+        .state('dashboard', {
+            url: '/',
+            templateUrl: 'partials/dashboard.html',
+            controller: 'DashboardCtrl'
+        })
+        
+        .state('users', {
+            url: '/users',
+            templateUrl: 'partials/users.html',
+            controller: 'UsersCtrl'
+        })
+        
+        .state('users.single', {
+            url: '/users/:id',
+            templateUrl: 'partials/users-single.html',
+            controller: 'UsersCtrl'
+        })
+        
+        .state('widgets', {
+            url: '/widgets',
+            templateUrl: 'partials/widgets.html',
+            controller: 'WidgetsCtrl'
+        })
+        
+        .state('widgets.single', {
+            url: '/widgets/:id',
+            templateUrl: 'partials/widgets-single.html',
+            controller: 'WidgetsCtrl'
+        });
+        
+        // redirect to dashboard if requested state is not defined
+        $urlRouterProvider.otherwise('/');
+        
+    }
+]);
+
+app.constant('API_URL', 'http://spa.tglrw.com:4000');
+
+app.run(['$rootScope',
+    function($rootScope) {
         
     }
 ]);
