be27567bf639c86684bd69464cd9cf9ad80f34e0
Jose Centeno <jmcenteno@gmail.com>
2015-08-18 15:45:20 -0400

    - angular module

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('WidgetSpa', [
+    'ui.router'
+]);
+
+app.constant('API_URL', 'http://spa.tglrw.com:4000');
+
+app.run(['$rootScope',
+    function($rootScope) {
+        
+        
+        
+    }
+]);
